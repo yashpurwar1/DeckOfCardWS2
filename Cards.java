4b55d3431fa32c9c482fea93ccf668f4b6a6a0be
yashpurwar1 <yashpurwar.yashpurwar.com>
2021-07-26 09:17:30 +0530

    Added: Initial setup for deck of card

--- Cards.java
+++ Cards.java
@@ -1,8 +1,19 @@
 package ws2;
-
+import java.util.*;
 public class Cards {
+	LinkedList<String> cards = new LinkedList<String>();
+	String[] suits = { "Diamond", "Heart", "Spades", "Club" };
+	String[] rank = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
 	
+	public void setcard() {
+		for(int i = 0; i <= 3; i++) {
+			for(int j = 0; j <= 12; j++ ) {
+				cards.add(suits[i]+" "+rank[j]);
+			}
+		}
+	}
 	public static void main(String[] args) {
-		System.out.println("Welcome to deck of card problem");
+		Cards card = new Cards();
+		card.setcard();
 	}
 }
